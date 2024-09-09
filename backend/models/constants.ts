3bf503804f000894925c15c5767312a5bdf2357d
Gabriel Cánepa <gacanepa@gmail.com>
2024-09-09 19:03:28 -0300

    Create constants file for models

---
+++ backend/models/constants.ts
@@ -0,0 +1,15 @@
+export const AFRICA = 'Africa';
+export const NORTH_AMERICA = 'North America';
+export const SOUTH_AMERICA = 'South America';
+export const ASIA = 'Asia';
+export const EUROPE = 'Europe';
+export const OCEANIA = 'Oceania';
+
+export const CONTINENTS = [AFRICA, NORTH_AMERICA, SOUTH_AMERICA, ASIA, EUROPE, OCEANIA];
+
+export const IN_OPERATION = 'In operation';
+export const ANNOUNCED = 'Announced';
+export const UNDER_CONSTRUCTION = 'Under construction';
+export const CLOSED_FOR_RENOVATION = 'Closed for renovation';
+
+export const STATUSES = [IN_OPERATION, ANNOUNCED, UNDER_CONSTRUCTION, CLOSED_FOR_RENOVATION];
