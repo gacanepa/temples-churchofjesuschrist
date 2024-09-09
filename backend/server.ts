9b92eec6e046e62713a4c3bf036a0a571d5dff96
Gabriel Cánepa <gacanepa@gmail.com>
2024-09-09 19:05:40 -0300

    Register temples router + JSON config

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import connectDatabase from './config/db.js';
+import templesRouter from './routes/templesRouter.js';
 
 dotenv.config();
 
@@ -8,10 +9,24 @@ const port: string = process.env.PORT ?? "5000";
 
 const app: Express = express();
 
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
   console.log(Object.keys(req));
-  connectDatabase();
   res.send('Hello World!');
 });
 
-app.listen(process.env.PORT, () => console.log(`Server running on port ${port}`));
+app.use('/api/temples', templesRouter);
+
+const start = async () => {
+  try {
+    await connectDatabase();
+    app.listen(process.env.PORT, () => console.log(`Server running on port ${port}`));
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    }
+  }
+};
+
+start();
