3748dfe4ea9c02692f04d183fd027119f3ed8d04
Gabriel Cánepa <gacanepa@gmail.com>
2024-11-25 18:35:45 -0300

    Update main server file

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import connect from './data/database.js';
+import indexRoutes from './routes/index.js';
+
+const app = express();
+
+dotenv.config();
+
+// Hide internal technical information
+app.disable('x-powered-by');
+
+// Request bodies will be sent as JSON
+app.use(express.json());
+
+const port = process.env.PORT || 3001;
+
+app.use('/', indexRoutes);
+// app.use('/temples', templesRoutes);
+// app.use('/docs', docsRoutes);
+
+app.listen(port, async () => {
+  await connect(process.env.MONGODB_URI);
+  console.log(`Server is running on port ${port}`);
+});
