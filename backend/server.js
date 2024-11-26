ef5a35a989baa866d9aeb7f779f66bbb7251fcee
Gabriel Cánepa <gacanepa@gmail.com>
2024-11-25 21:32:35 -0300

    Enable temple routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import connect from './data/database.js';
 import indexRoutes from './routes/index.js';
+import templesRoutes from './routes/temples.js';
 
 const app = express();
 
@@ -16,7 +17,7 @@ app.use(express.json());
 const port = process.env.PORT || 3001;
 
 app.use('/', indexRoutes);
-// app.use('/temples', templesRoutes);
+app.use('/temples', templesRoutes);
 // app.use('/docs', docsRoutes);
 
 app.listen(port, async () => {
