93cae1cc9e0acae4a439ff97faca3d100cb275dd
Gabriel Cánepa <gacanepa@gmail.com>
2024-12-04 22:45:14 -0300

    Add login and logout routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,20 @@
 import { Router } from 'express';
+import passport from 'passport';
 const router = Router();
 
 router.get('/', (_req, res) => {
   res.send('<h1>Welcome to the temples API</h1>');
 });
 
+router.get('/login', passport.authenticate('github'), (_req, _res) => {});
+
+router.get('/logout', (req, res) => {
+  req.logout(err => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
+});
+  
 export default router;
