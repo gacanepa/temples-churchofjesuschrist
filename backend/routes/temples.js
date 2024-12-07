824672ca95dd4cda92b518a8e0872e8732d62936
Gabriel Cánepa <gacanepa@gmail.com>
2024-12-07 12:12:33 -0300

    Use rate limit in all temple routes

--- backend/routes/temples.js
+++ backend/routes/temples.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import isAuthenticated from '../middleware/authenticate.js';
+import RateLimit from 'express-rate-limit';
 import {
   createTemple,
   getAllTemples,
@@ -7,8 +8,22 @@ import {
   softDeleteTemple,
   updateTemple,
 } from '../controllers/temples.js';
+import {
+  RATE_LIMIT_TIME_WINDOW,
+  RATE_LIMIT_MAX_REQUESTS,
+  RATE_LIMIT_MESSAGE,
+} from '../constants.js';
 
 const router = Router();
+
+// Configure rate limiter to prevent abuse of the API
+const limiter = new RateLimit({
+  windowMs: RATE_LIMIT_TIME_WINDOW,
+  max: RATE_LIMIT_MAX_REQUESTS,
+  message: RATE_LIMIT_MESSAGE,
+});
+
+router.use(limiter);
 
 router.get('/', isAuthenticated, getAllTemples);
 router.get('/:id', isAuthenticated, getTempleById);
