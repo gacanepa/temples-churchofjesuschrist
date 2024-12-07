b49a0d2f607985ac0a7ccd79916984098f84d03c
Gabriel Cánepa <gacanepa@gmail.com>
2024-12-07 12:59:26 -0300

    Add heading for message

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ import passport from 'passport';
 const router = Router();
 
 router.get('/', (req, res) => res.send(req.session.user !== undefined
-  ? `Hello, ${req.session.user.displayName}`
+  ? `<h1>Hello, ${req.session.user.displayName}</h1>`
   : '<h1>Welcome to the temples API</h1>'));
 
 router.get('/login', passport.authenticate('github'), (_req, _res) => {});
